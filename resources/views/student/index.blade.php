930e17deb03a7ba50fa15c5a320172fdcd82b8ba
malmosawig <malmosawig@gmail.com>
2022-08-11 20:12:22 +0300

    صفحة ارئيسية للطالب

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -38,7 +38,7 @@
                 <div class="card">
                     <div class="card-header ">
                         <h3 class="card-title ">Certificate</h3>
-                        @if ($certificate->degree != null)
+                        @if ($certificate != NULL)
                             <div class="card-options">
                                 <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
                                 <a href="{{route('student.printCertificate',Auth::guard('student')->user()->id)}}" class="card-options-collapse" ><i class="fa fa-print"></i></a>
