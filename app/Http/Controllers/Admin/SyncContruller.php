8ac029d8d63e14f6dc7e83857aeb412b4b18b5c4
malmosawig <malmosawig@gmail.com>
2022-08-15 12:04:24 +0300

    www

--- app/Http/Controllers/Admin/SyncContruller.php
+++ app/Http/Controllers/Admin/SyncContruller.php
@@ -56,7 +56,7 @@ class SyncContruller extends Controller
         //     $StudentExam->degree = $row->degree;
         // }
 
-        return response()->json(['$StudentExam'=> $request->StudentExam]);
+        return response()->json(['$StudentExam'=> $StudentExam]);
 
     }
 
