39716a7b62e6b4beb3c43e379b3ed35148e39f5f
malmosawig <malmosawig@gmail.com>
2022-08-18 22:16:47 +0300

    dddd

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('student.index');
-});
+Route::get('/', [StudentIndex::class, 'create']);
+
+// Route::get('/', function () {
+//     return view('student.index');
+// });
 Route::get('/demo', function () {
     return view('demo');
 });
