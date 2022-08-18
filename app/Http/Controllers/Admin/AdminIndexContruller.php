bdd8faf92aae6fd0507ec7e8ed02ddc0eeab64e2
malmosawig <malmosawig@gmail.com>
2022-08-18 13:07:50 +0300

    dd

--- app/Http/Controllers/Admin/AdminIndexContruller.php
+++ app/Http/Controllers/Admin/AdminIndexContruller.php
@@ -82,7 +82,7 @@ class AdminIndexContruller extends Controller
         // }
         // $governorate=Auth::guard('admin')->user()->governorate;
 
-        dd( Auth::guard('admin')->user()->governorate);
+        dd($governorate);
 
     }
     public function studentpass()
